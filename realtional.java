a2bf900d3ef3d5e1874369fbc29247305529cae6
prashans <prashans@prashanss-MacBook-Air.local>
2026-07-02 17:07:11 +0530

    udemyq1

---
+++ realtional.java
@@ -0,0 +1,8 @@
+public class realtional {
+    public static void main(String[] args){
+        int x=4;
+        int y=5;
+        boolean result=x<y;
+        System.out.println(result);
+    }
+}
