169b1b3e7a9de34771d79c93d1daa4b65929521e
prashans <prashans@prashanss-MacBook-Air.local>
2026-07-02 16:25:09 +0530

    udemyq1

---
+++ datatypes.java
@@ -0,0 +1,11 @@
+public class datatypes {
+    public static void main(String[] args){
+
+        int age = 25;
+        double height = 5.9;
+        String name = "Alex";
+
+        System.out.println("Name: " + name + ", Age: " + age + ", Height: " + height);
+    }
+    
+}
