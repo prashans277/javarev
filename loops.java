de73c72e34440d317ca3368c4a5bf55426e1717b
prashans277 <prashans027@gmail.com>
2026-07-03 13:22:10 +0530

    looprv

---
+++ loops.java
@@ -0,0 +1,9 @@
+public class loops {
+    public static void main(String[] args){
+        int i=1;
+        while(i<5){
+            System.out.println("hello");
+            i++;
+        }
+    }
+}
