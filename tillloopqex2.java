614045722341eba7484f17e3491b25cfe1917dd4
prashans277 <prashans027@gmail.com>
2026-07-03 16:59:59 +0530

    calculatorq2udmy

---
+++ tillloopqex2.java
@@ -0,0 +1,33 @@
+
+    public static void main(String[] args) {
+        
+        double num1 = 7;
+        double num2 = 3;
+        char operator = '+';
+        String again = "y"; 
+        
+        
+        
+        while(again.equals("y")){
+            if(operator == '+'){
+                System.out.println("Result: " + (num1 + num2));
+            }
+            else if(operator == '-'){
+                System.out.println("Result: " + (num1 - num2));
+            }
+            else if(operator == '*'){
+                System.out.println("Result: " + (num1*num2));
+            }
+            else if(operator == '/'){
+                System.out.println("Result: " + (num1/num2));
+            }
+        again="n";   
+            
+        }
+        
+        
+        System.out.println("Thank you for using the calculator.");
+    }
+    
+
+
