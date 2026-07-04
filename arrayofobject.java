aca38ab1152286ac5dd839101d017220a5ea58c6
prashans277 <prashans027@gmail.com>
2026-07-04 20:24:34 +0530

    arrayofobject

---
+++ arrayofobject.java
@@ -0,0 +1,33 @@
+class Student{
+    int rollno;
+    String name;
+    int marks;
+}
+public class arrayofobject {
+    public static void main(String[] args){
+        Student s1=new Student();
+        s1.rollno=1;
+        s1.name="amar";
+        s1.marks=50;
+
+        Student s2=new Student();
+        s2.rollno=2;
+        s2.name="Bharat";
+        s2.marks=55;
+
+        Student s3=new Student();
+        s3.rollno=3;
+        s3.name="Bharat";
+        s3.marks=65;
+
+        Student students[]=new Student[3];
+        students[0]=s1;
+        students[1]=s2;
+        students[2]=s3;
+
+        for(int i=0;i<students.length;i++){
+            System.out.println(students[i].rollno+" : "+students[i].name +" : "+students[i].marks);
+        }
+    }
+    
+}
