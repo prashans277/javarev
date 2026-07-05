b1d14fb34cc4a221d52a09148b020e9f60710b97
prashans277 <prashans027@gmail.com>
2026-07-06 00:26:30 +0530

    this and supermethod

---
+++ thisandsupermethod.java
@@ -0,0 +1,39 @@
+class A
+{
+    public A()
+    {
+        super();
+        System.out.println("in A");
+    }
+    public A(int n)
+    {
+        super();
+        System.out.println("in A int");
+    }
+}
+
+class B extends A
+{
+    public B()
+    {
+        super();
+        System.out.println("in B");
+    }
+
+    public B (int n)
+    {
+        super(n);
+        System.out.println("in B int");
+    }
+
+}
+
+
+
+public class thisandsupermethod {
+    public static void main(String[] args){
+
+        B obj=new B(5);
+    }
+    
+}
