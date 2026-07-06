f34601dccfbcd5324670243be6be83f0f61c90d4
prashans277 <prashans027@gmail.com>
2026-07-06 16:11:56 +0530

    methodoverriding

---
+++ methodoverriding.java
@@ -0,0 +1,24 @@
+class A {
+    public void show() {
+        System.out.println("in A show");
+    }
+
+    public void config() {
+        System.out.println("in A config");
+    }
+}
+
+class B extends A {
+    @Override
+    public void show() {
+        System.out.println("in B show");
+    }
+}
+
+public class methodoverriding {
+    public static void main(String[] args) {
+        B obj = new B();
+        obj.show();
+        obj.config();
+    }
+}
