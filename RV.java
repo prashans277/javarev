b72af623b0e04a2d6e98b851268598fffa13a1ff
prashans277 <prashans027@gmail.com>
2026-07-06 19:36:53 +0530

    reversethestring

---
+++ RV.java
@@ -0,0 +1,16 @@
+public class RV {
+    public static String reverseTheString(String str) {
+        StringBuilder reversed = new StringBuilder();
+
+        for (int i = str.length() - 1; i >= 0; i--) {
+            reversed.append(str.charAt(i));
+        }
+
+        return reversed.toString();
+    }
+
+    public static void main(String[] args) {
+        String str = "Hello";
+        System.out.println(reverseTheString(str));
+    }
+}
