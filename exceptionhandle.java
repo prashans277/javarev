16eebea6826337d52db5b05a39b494e422260849
prashans277 <prashans027@gmail.com>
2026-07-17 11:35:59 +0530

    exceptionhanlding

---
+++ exceptionhandle.java
@@ -0,0 +1,14 @@
+public class exceptionhandle {
+    public static void main(String[] args){
+        int i=0;
+        int j=0;
+        try{
+            j=18/i;
+        }
+        catch(Exception e){
+            System.out.println("something went wrong"+e);
+        }
+        System.out.println(j);
+        System.out.println("Byee. ");
+    }
+}
