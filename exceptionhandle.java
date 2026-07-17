6f60fae4174ce67d39b08e9f1b4021cb2850969c
prashans277 <prashans027@gmail.com>
2026-07-17 13:04:26 +0530

    exceptionhanldingquestoo

--- exceptionhandle.java
+++ exceptionhandle.java
@@ -1,14 +1,48 @@
-public class exceptionhandle {
-    public static void main(String[] args){
-        int i=0;
-        int j=0;
-        try{
-            j=18/i;
+// public class exceptionhandle {
+//     public static void main(String[] args){
+//         int i=0;
+//         int j=0;
+//         try{
+//             j=18/i;
+//         }
+//         catch(Exception e){
+//             System.out.println("something went wrong"+e);
+//         }
+//         System.out.println(j);
+//         System.out.println("Byee. ");
+//     }
+// }
+
+
+
+class InvalidAgeException extends Exception {
+    public InvalidAgeException(String message) {
+        super(message);
+    }
+}
+
+public class Exercise {
+
+    public static void checkAge(int age) throws InvalidAgeException {
+        if (age < 18) {
+            throw new InvalidAgeException("Age must be 18 or older");
+        } else {
+            System.out.println("Access granted");
         }
-        catch(Exception e){
-            System.out.println("something went wrong"+e);
+    }
+
+    public static void main(String[] args) {
+
+        try {
+            checkAge(16);
+        } catch (InvalidAgeException e) {
+            System.out.println("Exception caught: " + e.getMessage());
         }
-        System.out.println(j);
-        System.out.println("Byee. ");
+
+        try {
+            checkAge(21);
+        } catch (InvalidAgeException e) {
+            System.out.println("Exception caught: " + e.getMessage());
+        }
     }
 }
